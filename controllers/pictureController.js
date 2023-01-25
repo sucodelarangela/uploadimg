ed95fc60fb5a36293ba0bb844d0492146ce0acb0
Angela Caldas <aes.caldas@gmail.com>
2023-01-25 15:13:56 -0300

    feat: add GET route for finding all images

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -2,6 +2,7 @@ const Picture = require('../models/Picture');
 
 exports.create = async (req, res) => {
   try {
+    // name of the input on front end
     const { name } = req.body;
 
     const file = req.file;
@@ -16,5 +17,15 @@ exports.create = async (req, res) => {
     res.json({ picture, msg: 'Image successfully uploaded!' });
   } catch (error) {
     res.status(500).json({ message: 'Error when saving image.' });
+  }
+};
+
+exports.findAll = async (req, res) => {
+  try {
+    const pictures = await Picture.find();
+
+    res.json(pictures);
+  } catch (error) {
+    res.status(500).json({ message: 'Error when fetching images.' });
   }
 };
--- routes/picture.js
+++ routes/picture.js
@@ -8,5 +8,7 @@ const PictureController = require('../controllers/pictureController');
 
 // the upload goes as an intermediate method between access and creation
 router.post('/', upload.single('file'), PictureController.create);
+// get route to find all images
+router.get('/', PictureController.findAll);
 
 module.exports = router;
